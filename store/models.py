07060ab42ae1292639d4e22410519122ebfa4aa2
Roshankdk <roshankdk.27@gmail.com>
2024-05-15 19:17:06 +0545

    Models Created

--- store/models.py
+++ store/models.py
@@ -1,3 +1,44 @@
+import datetime
 from django.db import models
+from django.db.models.signals import ModelSignal
+from django.views.generic import dates
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default = 0, decimal_places=2, max_digits=10)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=250, default='',blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/product/')
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    quantaty = models.IntegerField(default=1)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=10)
+    date = models.DateTimeField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.product
+
