0f90df4d5ae958a31df7329ec3843017e5fb1c71
Roshankdk <roshankdk.27@gmail.com>
2024-05-19 21:23:47 +0545

    Added cart template and related urls

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -21,5 +21,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('',include('store.urls')),
+    path('cart/',include('cart.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
