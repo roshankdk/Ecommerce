4749f2bc1e3cdaf46150b5ede26ab96bdb320df7
Roshankdk <roshankdk.27@gmail.com>
2024-05-25 20:53:37 +0545

    cart/urls modified

--- cart/urls.py
+++ cart/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('add/',views.cart_add,name='cart_add'),
     path('delete/',views.cart_delete,name='cart_delete'),
     path('update/',views.cart_update,name='cart_update'),
-
+    path('setSession/',views.setSession,name='setSession'),
+    path('getSession/',views.getSession,name='getSession'),
 ]
