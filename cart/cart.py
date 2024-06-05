c590a809b2460916ab1ae760683acc123377d274
Roshankdk <roshankdk.27@gmail.com>
2024-06-05 21:28:19 +0545

    Message for cart

--- cart/cart.py
+++ cart/cart.py
@@ -2,39 +2,43 @@ from django.db.models import constraints
 from django.http import request
 from store.models import Product
 
-class Cart():
+
+class Cart:
     def __init__(self, request) -> None:
         self.session = request.session
-        cart = self.session.get('cart')
+        cart = self.session.get("cart")
         if not cart:
-            cart = self.session['cart'] = {}
+            cart = self.session["cart"] = {}
         self.cart = cart
-    
-    def add(self,product,itemQty):
+
+    # function to add item in cart
+    def add(self, product, itemQty):
         product_id = str(product.id)
         itemQty = str(itemQty)
 
         if product_id not in self.cart:
-            self.cart[product_id] = (itemQty)
-                
+            self.cart[product_id] = itemQty
+
         self.session.modified = True
 
-    def delete(self,productId):
+    # function to remove item from cart
+    def delete(self, productId):
         productId = str(productId)
         del self.cart[productId]
-    
+
         self.session.modified = True
 
+    # returns the length of the cart i.e. total items in the cart
     def __len__(self):
         return len(self.cart)
 
+    # return the products in the cart
     def get_cart_product(self):
         # product_ids = list(map(int, self.cart.keys()))
         product_ids = self.cart.keys()
         product = Product.objects.filter(id__in=product_ids)
         return product
-    
+
     def get_itemQty(self):
         itemQty = self.cart
         return itemQty
-
--- cart/views.py
+++ cart/views.py
@@ -9,35 +9,39 @@ from django.http import HttpResponse, JsonResponse, response
 
 # Create your views here.
 
+
 def cart_summary(request):
     cart = Cart(request)
     cart_products = cart.get_cart_product()
     itemQty = cart.get_itemQty
-    return render(request,'cart/cart_summary.html',{'cart_products':cart_products, 'itemQty':itemQty})
+    context = {"cart_products": cart_products, 
+               "itemQty": itemQty}
+    return render(request,"cart/cart_summary.html",context)
+
 
 def cart_add(request):
     cart = Cart(request)
-    if request.POST.get('action') == 'post':
-        product_id = int(request.POST.get('product_id'))
-        itemQty = int(request.POST.get('itemQty'))
+    if request.POST.get("action") == "post":
+        product_id = int(request.POST.get("product_id"))
+        itemQty = int(request.POST.get("itemQty"))
         product = get_object_or_404(Product, id=product_id)
-        cart.add(product=product, itemQty = itemQty)
-        cart_quantity = cart.__len__()  
-        response = JsonResponse({'qty': cart_quantity})
+        cart.add(product=product, itemQty=itemQty)
+        cart_quantity = cart.__len__()
+        response = JsonResponse({"qty": cart_quantity})
+        messages.success(request, ("Item added to the cart!!"))
         return response
-
 
 
 def cart_update(request):
     pass
-    
+
 
 def cart_delete(request):
     cart = Cart(request)
-    if request.POST.get('action') == 'post':
-        productId = request.POST.get('productId')
+    if request.POST.get("action") == "post":
+        productId = request.POST.get("productId")
         cart.delete(productId=productId)
         cart_quantity = cart.__len__()
-        response = JsonResponse({'qty':cart_quantity})
+        response = JsonResponse({"qty": cart_quantity})
+        messages.success(request, ("Item removed from the cart!!"))
         return response
-
